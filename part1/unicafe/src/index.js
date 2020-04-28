d92700b62973ff36994c5304cc70f64eb34d2c35
Onur Temiz <onurmain@gmail.com>
2020-04-28 15:51:30 +0300

    Exercise 1.7 & 1.8 Done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -11,7 +11,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const infoArr = [good, neutral, bad];
-
+  const totalFeedback = good + bad + neutral;
   const setToGood = () => {
     setGood(good + 1);
   };
@@ -31,17 +31,51 @@ const App = () => {
       <Button handleClick={setToNeutral} text={neutral_title} />
       <Button handleClick={setToBad} text={bad_title} />
       <SecondTitle second_title={second_title} />
-      <Statistics info={infoArr} />
+      <Statistics
+        info={infoArr}
+        good={good}
+        bad={bad}
+        totalFeedback={totalFeedback}
+      />
     </div>
   );
 };
 
-const Statistics = ({ info }) => {
+const Positive = ({ good, all }) => {
+  return (
+    <div>
+      <p>positive {(good / all) * 100} %</p>
+    </div>
+  );
+};
+
+const All = ({ all }) => {
+  return (
+    <div>
+      <p>all {all}</p>
+    </div>
+  );
+};
+
+const Average = ({ all, good, bad }) => {
+  return (
+    <div>
+      <p>average {(good - bad) / all}</p>
+    </div>
+  );
+};
+
+const Statistics = ({ info, totalFeedback, good, bad }) => {
+  console.log(totalFeedback);
   return (
     <div>
       <p>good: {info[0]}</p>
       <p>neutral: {info[1]}</p>
       <p>bad: {info[2]}</p>
+
+      <All all={totalFeedback} />
+      <Average all={totalFeedback} good={good} bad={bad} />
+      <Positive good={good} all={totalFeedback} />
     </div>
   );
 };
