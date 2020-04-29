82ec74fadc067e41cb577490490733b3c5130532
Onur Temiz <onurmain@gmail.com>
2020-04-29 10:15:51 +0300

    Exercise 2.5 Done

--- part2/coursecontents/src/App.js
+++ part2/coursecontents/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Course from "./components/course";
 
 const App = () => {
   const courses = [
@@ -51,57 +52,6 @@ const App = () => {
       {courses.map((course) => (
         <Course course={course} key={course.id} />
       ))}
-    </div>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  return (
-    <div>
-      <p>
-        <b>
-          total of {parts.reduce((sum, current) => sum + current.exercises, 0)}{" "}
-          exercises
-        </b>
-      </p>
-    </div>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part part={part} key={part.id} />
-      ))}
-    </div>
-  );
-};
-
-const Part = ({ part }) => {
-  return (
-    <div>
-      <p>
-        {part.name} {part.exercises}
-      </p>
-    </div>
-  );
-};
-
-const Header = ({ name }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
     </div>
   );
 };
