e57d2ca2aa48e9d6a0e81e698c6ea0f58b525c9d
Onur Temiz <onurmain@gmail.com>
2020-05-01 12:11:30 +0300

    Exercise 4.2 Done

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,70 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+// GET ALL BLOGS
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs.map((blog) => blog.toJSON()));
+  });
+});
+
+// GET ONE BLOG
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id).then((blog) => {
+    response.json(blog.toJSON());
+  });
+  // .catch((error) => next(error));
+});
+
+// DELETE ONE BLOG
+blogsRouter.delete("/:id", (request, response) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+// POST ONE BLOG
+blogsRouter.post("/", (request, response, next) => {
+  if (
+    !request.body.title ||
+    !request.body.author ||
+    !request.body.url ||
+    !request.body.likes
+  ) {
+    return response.status(400).json({
+      error: "content is missing",
+    });
+  }
+
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result.toJSON());
+    })
+    .catch((error) => next(error));
+});
+
+// CHANGE ONE BLOG
+blogsRouter.put("/:id", (request, response, next) => {
+  const { body } = request;
+  newBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, newBlog, {
+    new: true,
+  })
+    .then((blog) => {
+      response.json(blog.toJSON());
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogsRouter;
