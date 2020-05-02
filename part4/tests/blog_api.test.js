56d369fc80c6a88400a94691dde44b0fdfbfbdb8
Onur Temiz <onurmain@gmail.com>
2020-05-02 17:37:36 +0300

    Exercise 4.22 Done

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -5,100 +5,164 @@ const api = supertest(app);
 const helper = require("./test_helper");
 const Blog = require("../models/blog");
 
-console.log("test started");
 beforeEach(async () => {
   await Blog.deleteMany({});
-  console.log("old entry deleted");
   const blogObjects = helper.blogs.map((blog) => new Blog(blog));
   const promiseArray = blogObjects.map((blog) => blog.save());
   await Promise.all(promiseArray);
-  console.log("new entries are added");
+});
+
+describe("something", () => {
+  test("all blogs are returned as json", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(helper.blogs.length);
+  });
+
+  test("id must be defined for every item", async () => {
+    const response = await api.get("/api/blogs");
+    response.body.forEach((blog) => {
+      expect(blog.id).toBeDefined();
+    });
+  });
+
+  test("new item can added to", async () => {
+    const newBlog = new Blog({
+      title: "Neler oluyor neler",
+      author: "Ahmet Mehmet",
+      url: "cokgizlibirurlbidiribibd",
+      likes: 102,
+    });
+
+    const user = {
+      username: "xdasxdasdxdxdxd",
+      name: "kokok",
+      password: "asdaassdasdasd",
+    };
+    await api.post("/api/users").send(user).expect(201);
+
+    const userToken = await api.post("/api/login").send({
+      username: user.username,
+      password: user.password,
+    });
+
+    await api
+      .post("/api/blogs")
+      .set("Authorization", "bearer " + userToken.body.token)
+      .send(newBlog);
+    const blogsNew = await helper.blogsInDb();
+    expect(blogsNew).toHaveLength(helper.blogs.length + 1);
+    const authors = blogsNew.map((blog) => blog.author);
+    expect(authors).toContain("Ahmet Mehmet");
+  });
+
+  test("if no like specified, 0 likes created", async () => {
+    const newBlog = new Blog({
+      title: "nelerlerelre",
+      author: "burasicokonemli",
+      url: "huehuehuehueuhe",
+    });
+
+    const user = {
+      username: "xasdxdxdxd",
+      name: "kokok",
+      password: "adasdasd",
+    };
+    await api.post("/api/users").send(user).expect(201);
+
+    const userToken = await api.post("/api/login").send({
+      username: user.username,
+      password: user.password,
+    });
+
+    const response = await api
+      .post("/api/blogs")
+      .set("Authorization", "bearer " + userToken.body.token)
+      .send(newBlog);
+    expect(response.body.likes).toBeDefined();
+    expect(response.body.likes).toEqual(0);
+  });
+
+  test("url must be present", async () => {
+    const newBlog = new Blog({
+      author: "burasicokonemli",
+      title: "huehuehuehueuhe",
+    });
+
+    const user = {
+      username: "qxdxdasdxdxdxd",
+      name: "kokok",
+      password: "qasdasdasdasd",
+    };
+    await api.post("/api/users").send(user).expect(201);
+
+    const userToken = await api.post("/api/login").send({
+      username: user.username,
+      password: user.password,
+    });
+    await api
+      .post("/api/blogs")
+      .set("Authorization", "bearer " + userToken.body.token)
+      .send(newBlog)
+      .expect(400);
+  });
+
+  test("title must be present", async () => {
+    const newBlog = new Blog({
+      author: "burasicokonemli",
+      url: "huehuehuehueuhe",
+    });
+
+    const user = {
+      username: "xdxdasssqdxdxdxd",
+      name: "kokok",
+      password: "asdasdasaadasd",
+    };
+    await api.post("/api/users").send(user).expect(201);
+
+    const userToken = await api.post("/api/login").send({
+      username: user.username,
+      password: user.password,
+    });
+    await api
+      .post("/api/blogs")
+      .set("Authorization", "bearer " + userToken.body.token)
+      .send(newBlog)
+      .expect(400);
+  });
+
+  test("update a single item", async () => {
+    const newBlog = new Blog({
+      title: "nelerlerelre",
+      author: "burasicokonemli",
+      url: "huehuehuehueuhe",
+    });
+
+    const user = {
+      username: "xdxdxfdxdxd",
+      name: "kokok",
+      password: "asdasfdasd",
+    };
+    await api.post("/api/users").send(user).expect(201);
+
+    const userToken = await api.post("/api/login").send({
+      username: user.username,
+      password: user.password,
+    });
+
+    const resBlog = await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .set("Authorization", "bearer " + userToken.body.token);
+    resBlog.body.likes = 20;
+
+    finalBlog = await api
+      .put(`/api/blogs/${resBlog.body.id}`)
+      .send(resBlog.body)
+      .set("Authorization", "bearer " + userToken.body.token);
+    expect(resBlog.body.likes).toEqual(finalBlog.body.likes);
+  });
 });
 
 afterAll(() => {
   mongoose.connection.close();
-});
-
-test("all blogs are returned as json", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(helper.blogs.length);
-});
-
-test("id must be defined for every item", async () => {
-  const response = await api.get("/api/blogs");
-  response.body.forEach((blog) => {
-    expect(blog.id).toBeDefined();
-  });
-});
-
-test("new item can added to", async () => {
-  const newBlog = new Blog({
-    title: "Neler oluyor neler",
-    author: "Ahmet Mehmet",
-    url: "cokgizlibirurlbidiribibd",
-    likes: 102,
-  });
-
-  await api.post("/api/blogs").send(newBlog);
-  const blogsNew = await helper.blogsInDb();
-  expect(blogsNew).toHaveLength(helper.blogs.length + 1);
-  const authors = blogsNew.map((blog) => blog.author);
-  expect(authors).toContain("Ahmet Mehmet");
-});
-
-test("if no like specified, 0 likes created", async () => {
-  const newBlog = new Blog({
-    title: "nelerlerelre",
-    author: "burasicokonemli",
-    url: "huehuehuehueuhe",
-  });
-
-  const response = await api.post("/api/blogs").send(newBlog);
-  expect(response.body.likes).toBeDefined();
-  expect(response.body.likes).toEqual(0);
-});
-
-test("url must be present", async () => {
-  const newBlog = new Blog({
-    author: "burasicokonemli",
-    title: "huehuehuehueuhe",
-  });
-  await api.post("/api/blogs").send(newBlog).expect(400);
-});
-
-test("title must be present", async () => {
-  const newBlog = new Blog({
-    author: "burasicokonemli",
-    url: "huehuehuehueuhe",
-  });
-
-  await api.post("/api/blogs").send(newBlog).expect(400);
-});
-
-test("update a single item", async () => {
-  const newBlog = new Blog({
-    title: "nelerlerelre",
-    author: "burasicokonemli",
-    url: "huehuehuehueuhe",
-  });
-
-  const resBlog = await api.post("/api/blogs").send(newBlog);
-
-  resBlog.body.likes = 20;
-
-  finalBlog = await api.put(`/api/blogs/${resBlog.body.id}`).send(resBlog.body);
-  expect(resBlog.body.likes).toEqual(finalBlog.body.likes);
-});
-
-test("delete an item", async () => {
-  const dbAtStart = await helper.blogsInDb();
-  const blogAtStart = dbAtStart[0];
-
-  await api.delete(`/api/blogs/${blogAtStart.id}`).expect(204);
-
-  const dbAtEnd = await helper.blogsInDb();
-
-  expect(dbAtEnd).toHaveLength(dbAtStart.length - 1);
-  const titles = dbAtEnd.map((blog) => blog.title);
-  expect(titles).not.toContain(blogAtStart.title);
 });
